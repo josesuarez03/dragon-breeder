4b7a1b0d41bc5c6d8cc38faffc91b227330924d8
XxIMDARIOxX <dariospain1132@gmail.com>
2024-09-26 10:09:54 +0200

    Added box-eggs(mistery box) and the backend in app.js

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const expressLayouts = require('express-ejs-layouts');
 const characterController = require('./controllers/characterController');
 const gameController = require('./controllers/gameController'); 
+const { saveCharacters } = require('./models/characterModel');
 const app = express();
 const PORT = 3000;
 
@@ -13,6 +14,14 @@ app.set('layout', 'layout');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json()); // Permitir JSON en el cuerpo de las solicitudes
 app.use(express.static('public'));
+
+
+if (!saveCharacters){
+    app.get('/', (req, res) => {
+        const selectedEgg = getRandomEgg();
+        res.render('box-eggs', {selectedEgg});
+    });
+}
 
 // Root route
 app.get('/', (req, res) => {
@@ -32,6 +41,21 @@ app.get('/game', gameController.view);
 app.get('/game/select', gameController.select);
 app.post('/game/select', gameController.chooseCharacter);
 app.put('/game/update', gameController.updateEnergy); 
+
+
+function getRandomEgg(){
+    const random = Math.random() * 100;
+
+    if (random <= 10) {
+        return 1; // Huevo raro (10%)
+    } else if (random <= 40) {
+        return 2; // Huevo común (30%)
+    } else if (random <= 75) {
+        return 3; // Huevo común (30%)
+    } else {
+        return 4; // Huevo común (30%)
+    }
+}
 
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
